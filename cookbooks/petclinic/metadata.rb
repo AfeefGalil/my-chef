c89472e0d812bb8788c9f48cd7a244950a151d62
Afeef <afeefaz@Afeefs-MacBook-Pro.local>
2022-05-03 15:02:28 +0300

    init

---
+++ cookbooks/petclinic/metadata.rb
@@ -0,0 +1,10 @@
+name 'petclinic'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'All Rights Reserved'
+description 'Installs/Configures petclinic'
+version '0.1.0'
+chef_version '>= 16.0'
+
+depends 'aws', '~> 9.0.2'
+depends 'java', '~> 11.1.0'
---
+++ solo.rb
@@ -0,0 +1,2 @@
+chef_repo_path '/Users/afeefaz/Desktop/Chef/afeef_petclinic'
+cookbook_path ['/Users/afeefaz/Desktop/Chef/afeef_petclinic/cookbooks', '/Users/afeefaz/Desktop/Chef/afeef_petclinic/site_cookbooks']
