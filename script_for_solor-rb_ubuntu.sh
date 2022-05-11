55aa0c9821298d26eca49e6fbea5cbd96a4a64a0
Afeef <afeefaz@Afeefs-MacBook-Pro.local>
2022-05-11 14:50:05 +0300

    fixes

--- script_for_solor-rb_ubuntu.sh
+++ script_for_solor-rb_ubuntu.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-echo "chef_repo_path '/home/ubuntu/my-chef/afeef_petclinic'" > solo.rb
-echo "cookbook_path '/home/ubuntu/my-chef/afeef_petclinic/site_cookbooks'" >> solo.rb
+echo "chef_repo_path '/home/ubuntu/my-chef'" > solo.rb
+echo "cookbook_path '/home/ubuntu/my-chef/site_cookbooks'" >> solo.rb
 echo "role_path 'cookbooks/petclinic/roles'" >> solo.rb
