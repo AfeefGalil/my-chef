d5bf84d0016ec0153e6edd44a8c273f54f87605e
Afeef <afeefaz@Afeefs-MacBook-Pro.local>
2022-05-15 11:15:48 +0300

    fix

--- cookbooks/petclinic/recipes/default.rb
+++ cookbooks/petclinic/recipes/default.rb
@@ -38,7 +38,7 @@ case $1 in
 start)
   echo 'Starting $SERVICE_NAME ...'
 if [ ! -f $PID_PATH_NAME ]; then
-  nohup java -jar $PATH_TO_JAR --spring.profiles.active=mysql /tmp 2>> /dev/null >>/dev/null &
+  nohup java -jar $PATH_TO_JAR --spring.profiles.active=mysql >> /dev/null &
               echo $! > $PID_PATH_NAME
   echo '$SERVICE_NAME started ...'
 else
@@ -64,7 +64,7 @@ if [ -f $PID_PATH_NAME ]; then
   echo '$SERVICE_NAME stopped ...';
   rm $PID_PATH_NAME
   echo '$SERVICE_NAME starting ...'
-  nohup java -jar $PATH_TO_JAR --spring.profiles.active=mysql /tmp 2>> /dev/null >> /dev/null &
+  nohup java -jar $PATH_TO_JAR --spring.profiles.active=mysql /dev/null &
   echo $! > $PID_PATH_NAME
   echo '$SERVICE_NAME started ...'
 else
